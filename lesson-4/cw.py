7557804bbf21b1ca01cbd3b59e47cd499a9d0e22
giorgi2008-ux <gpapidze2008@gmail.com>
2025-09-04 16:14:14 +0400

    დავალებები ატვირტე

---
+++ lesson-4/cw.py
@@ -0,0 +1,48 @@
+
+# +
+# -
+# // , /
+# *
+# **
+# %
+
+print(20+3)
+print(5+2)
+print(6+90)
+print(8+4)
+print(7+39)
+print(20-3)
+print(20-4)
+print(46-18)
+print(58-36)
+print(80-47)
+print(20//3)
+print(90//2)
+print(8//3)
+print(89/34)
+print(67//23)
+print(20/3)
+print(6/5)
+print(98/3)
+print(8/4)
+print(65/2)
+print(20*3)
+print(40*6)
+print(30*5)
+print(5*5)
+print(7*8)
+print(20**3)
+print(90**8)
+print(3**6)
+print(90**59)
+print(23**7)
+print(20%3)
+print(6%8)
+print(3%98)
+print(212%98)
+print(45%5)
+#  % 2
+# -
+# 18
+# -------
+# 2
