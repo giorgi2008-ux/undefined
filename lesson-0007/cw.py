d849cdca0f3e27677d666dc5776cac6b81f6a6f8
giorgi2008-ux <gpapidze2008@gmail.com>
2025-09-07 00:11:29 +0400

    linxui magaria

---
+++ lesson-0007/cw.py
@@ -0,0 +1,11 @@
+number=20
+isEven="is number even? " + str(number % 2 == 0)
+isOdd="is number odd? " + str(number % 2 == 1)
+
+print(isEven)
+print(isOdd)
+
+
+
+# logical operators
+# and,or,       not
---
+++ lrsoooonn/app.py
@@ -0,0 +1,8 @@
+number= 20
+print(number/2)
+
+if number % 20 == 0:
+    print(True)
+
+else:
+    print(False)
