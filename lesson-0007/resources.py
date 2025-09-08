c422fa357f65e038c167a7d209d647fb35d8c88d
giorgi2008-ux <gpapidze2008@gmail.com>
2025-09-08 16:22:05 +0400

    linxui magaria

---
+++ lesson-0007/resources.py
@@ -0,0 +1,46 @@
+number=20
+isEven="is number even? " + str(number % 2 == 0)
+isOdd="is number odd? " + str(number % 2 == 1)
+
+print(isEven)
+print(isOdd)
+
+
+
+# logical operators
+# and,or,    not
+
+
+
+print(False and False) #false         
+print(3==3 and 3!=0) #true
+print(False or False) #false
+print(True or True) #true
+print(True or False) #true
+print(False or True) #true
+
+print(True and True) #true
+print(True and False) #false
+print(False and True) #false
+print(False and False) #false
+
+
+#condition/მდგომარეობა 
+
+# if else
+if False:
+    print("swoira")
+else:
+    print("arasworia")
+    
+#(-------------------------)
+
+if 35>27:
+    print("yochag")
+else:
+    print("meti ara ia kide")
+
+
+ #---indentaion error/კედლიდან დაშორება---
+
+
