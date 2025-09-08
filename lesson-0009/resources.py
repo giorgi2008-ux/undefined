4b9c4007d14bef0cceb32c0800c0ec1b707a06c4
giorgi2008-ux <gpapidze2008@gmail.com>
2025-09-08 23:40:40 +0400

    if else elif

---
+++ lesson-0009/resources.py
@@ -0,0 +1,13 @@
+if 4 > 5 :
+    print("lomfxvi")
+
+elif 4 == 5: #ვწერთ რადესაც გვინდა
+    print("zviangi")
+
+elif 6 - 4 == 10:
+    print("topopi")
+
+else:
+    print("vampiriii")
+
+# თუ არც if-ს და არც elif-ს პირობა არ შესრულდა,გამოიტანს else-ს პირობას.
