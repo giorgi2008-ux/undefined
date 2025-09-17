7ace9d17a74a6f0d1bbe1777870127dc2fb45643
giorgi2008-ux <gpapidze2008@gmail.com>
2025-09-17 11:47:46 +0400

    lomi

---
+++ lesson-0013/cw.py
@@ -0,0 +1,9 @@
+
+for i in range(10):
+    if i % 2 == 0:
+        print( "luwia"+str(i))ff
+    else:
+        print("kentia"+str(i))
+
+git config --global user.email "gpapidze2008@gmail.com"
+  git config --global user.name "giorgi2008-ux"
---
+++ lesson-0015/resources.py
@@ -0,0 +1,15 @@
+i=0
+while i < 3:
+    print("lomfxvi")
+    i=i+1
+
+
+i=0
+while i < 3:
+    print("lomfxvi")
+    i +=1
+
+# i = i + 1 and i += 1 it's same
+# new data type list / array  სია | მასივი  
+name_list = ["vefxvi", "kata", "dzagli" ]
+print(type(name_list))
