32e8cab6db61afdad3eb52af99d238ac7c9f3afe
giorgi2008-ux <gpapidze2008@gmail.com>
2025-09-28 16:05:32 +0400

    wjorng

--- lesson-0021/cw.py
+++ lesson-0021/cw.py
@@ -9,33 +9,32 @@ array=[1,2,3,"lomi","vefxvi","spilo",[43,33],False,True]
 print("----------")
 # ---------------
 
-for i in range(len(array)):
+for i in range(len(array)): #გამოიტანს array-ს ყველა ელემენტას.
     print(array[i])
 print("----------")
 # ---------------
 
-for i in range(len(array)):
+for i in range(len(array)):# გამოიტანს array-ს ელემენტების რაოდენობას.
     print(i)
 print("----------")
-# ---------------
+# # ---------------
 
-for i in range(7):
+for i in range(7): # გამოიტანს ციფრებს 1-დან 7-მდე.
     print(i)
 print("----------")
 
 i=0
-while i < (len(array)):
+while i < (len(array)):# სანამ i არ გახდება array-ს ელემენტების ტოლი, მანამდე i  ყოვენ ბრუნვაზე დაემატება 1 ანუ i+=1
     print(array[i])
     i+=1
-print("----------")
+# print("----------")
 
 slice_array=[1,2,3,4,5,6,7,8,9,0]
-print(slice_array[0:5])
-print(slice_array[5:])
-print(slice_array[:8])
-print(slice_array[::])
-print(slice_array[::5])
-print(slice_array[:8:2])
+print(slice_array[0:5]) # გამოიტანს თვიდან დან მე-5-ე ინდექსამდე
+print(slice_array[5:]) # გამოიტანს მე-5-ე ინდექსიდან ბოლომდე
+print(slice_array[:8]) # გამოიტანს თვიდან მე-8-ე ინდექსამდე
+print(slice_array[::]) # გამოიტანს მთლიანად
+print(slice_array[::5]) # გამოიტანს ყოველ მე-5-ე ინდექსს
+print(slice_array[:8:2]) # გამოიტანს თვიდან მე-8-ე ინდექსამდე, ყოველ მე-2-ე ინდექსს
 print(slice_array[::1]) # დაიწყე თავიდან ბოლომდე
 print(slice_array[::-1]) #დაიწყე ბოლოდან თვამდე
-
