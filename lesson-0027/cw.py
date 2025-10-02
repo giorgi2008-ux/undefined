5bc979b75fbefcfee28c8f6300430878f31d5541
giorgi2008-ux <gpapidze2008@gmail.com>
2025-10-02 14:55:39 +0400

    gogjoew

---
+++ lesson-0027/cw.py
@@ -0,0 +1,8 @@
+# new_list = []
+# for i in range(new_list:
+#     print(len(new_list))
+    
+a=[]
+for i in range(100):
+    a.append(i)
+print(a)
--- lesson-0027/resource.py
+++ lesson-0027/resource.py
@@ -1,8 +1,12 @@
-# new_list = []
-# for i in range(new_list:
-#     print(len(new_list))
-    
-a=[]
-for i in range(100):
-    a.append(i)
-    print(a)
+num=[1,2,3]
+print(num)
+num[0]=3
+num[1]=4
+num[2]=5
+print(num)
+
+print("-----------")
+
+name="giorgi"
+for i in range(0,len(name),2): # შეგვიძლია slicing გამოვიყენოთ for ციკლთან ერთდ. ამ შემთხვევში , ასრულებს :-ს ფუნქციას და გამოგვიტანს name-ს ყოველ მე-2 ასოს. 
+    print(name[i])
